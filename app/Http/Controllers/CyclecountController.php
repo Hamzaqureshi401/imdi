b00d0e68050c5eb2b2816ba1c7a193cd8434632d
Hamzaqureshi401 <46222436+Hamzaqureshi401@users.noreply.github.com>
2023-10-25 22:56:40 +0500

    permission set

--- app/Http/Controllers/CyclecountController.php
+++ app/Http/Controllers/CyclecountController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Cyclecount;
 use Illuminate\Http\Request;
 use App\Models\Cyclecountlocation;
+use App\Models\Permission;
 use Illuminate\Support\Facades\Auth;
 
 
