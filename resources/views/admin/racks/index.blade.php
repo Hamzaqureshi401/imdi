f5ea1643941833c06cb067dde44a82b98c287edb
Hamzaqureshi401 <46222436+Hamzaqureshi401@users.noreply.github.com>
2023-11-29 14:20:05 +0500

    print error set

--- resources/views/admin/racks/index.blade.php
+++ resources/views/admin/racks/index.blade.php
@@ -159,8 +159,9 @@ Manage Warehouse Racks
       </div>
    </div>
 </div>
-@endsection
-@section('footer')
+
+<script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+
 <script>
    $(document).ready(function() {
      // Attach a submit event handler to the form
